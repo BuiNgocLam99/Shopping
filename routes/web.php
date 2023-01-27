80a73b07f98d8057d1941b74d6cdebe61db3db57
VuaLiDon <lambn957@gmail.com>
2023-01-27 22:54:15 +0700

    Admin Category Page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
@@ -60,4 +61,6 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'authadmin'])->group(function () {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)
         ->name('admin.dashboard');
+    Route::get('/admin/categories', AdminCategoryComponent::class)
+        ->name('admin.categories');
 });
