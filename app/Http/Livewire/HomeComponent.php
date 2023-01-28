31339b3458272dffcb2640e0f097c933bc69a27c
VuaLiDon <lambn957@gmail.com>
2023-01-28 15:21:05 +0700

    Admin Show Latest Products On Homepage

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\HomeSlider;
+use App\Models\Product;
 use Livewire\Component;
 
 class HomeComponent extends Component
@@ -10,7 +11,8 @@ class HomeComponent extends Component
     public function render()
     {
         $sliders = HomeSlider::where('status', 1)->get();
-        return view('livewire.home-component', compact('sliders'))
+        $latest_products = Product::orderBy('created_at', 'DESC')->get()->take(8);
+        return view('livewire.home-component', compact('sliders', 'latest_products'))
             ->layout('layouts.base');
     }
 }
