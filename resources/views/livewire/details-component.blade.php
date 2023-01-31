f0d4f27155aaf817594d390223cbbeacd9210ee1
VuaLiDon <lambn957@gmail.com>
2023-01-31 17:26:40 +0700

    Move Product From Wishlist to Cart and Make Quantity Working on Details Page

--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -96,10 +96,10 @@
                         <div class="quantity">
                             <span>Quantity:</span>
                             <div class="quantity-input">
-                                <input type="text" name="product-quatity" value="1" data-max="120" pattern="[0-9]*" >
+                                <input type="text" wire:model="qty" name="product-quatity" value="1" data-max="120" pattern="[0-9]*" >
                                 
-                                <a class="btn btn-reduce" href="#"></a>
-                                <a class="btn btn-increase" href="#"></a>
+                                <a wire:click.prevent="decreaseQuantity" class="btn btn-reduce" href="#"></a>
+                                <a wire:click.prevent="increaseQuantity" class="btn btn-increase" href="#"></a>
                             </div>
                         </div>
                         <div class="wrap-butons">
--- resources/views/livewire/wishlist-component.blade.php
+++ resources/views/livewire/wishlist-component.blade.php
@@ -41,7 +41,7 @@
                             <div class="product-info">
                                 <a href="{{ route('product.details', $item->model->slug) }}" class="product-name"><span>{{ $item->model->name }}</span></a>
                                 <div class="wrap-price"><span class="product-price">${{ $item->model->regular_price }}</span></div>
-                                <a href="#" wire:click="store({{ $item->model->id }}, '{{ $item->model->name }}', {{ $item->model->regular_price }})" class="btn add-to-cart">Add To Cart</a>
+                                <a href="#" wire:click="moveProductFromWishlistToCart('{{ $item->rowId }}')" class="btn add-to-cart">Move To Cart</a>
                                 <div class="product-wish">
                                     <a href="#" wire:click.prevent="removeFromWishlist({{ $item->model->id }})"><i class="fa fa-heart fill-heart"></i></a>
                                 </div>
